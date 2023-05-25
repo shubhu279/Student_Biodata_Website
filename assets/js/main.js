502383ed352b6ac8e8ef17bc8d769aa0e9526800
Saptarshi <saptarshimemari072@gmail.com>
2023-05-25 17:15:36 +0530

    New Looking Homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-console.log("Welcome Folks!!!");
+// console.log("Welcome Folks!!!");
 
+/*Moving the Nav bar -- will use this later in other pages*/
 
-let nav = document.querySelector(".nav");
-window.onscroll = ()=>{
-    if(document.documentElement.scrollTop >= 100)
-    {
-        nav.style.top = "0px";
-        nav.style.position = "sticky";
-        nav.style.transition = "0.4s ease-in"
-    }
-    else{
-        nav.style.top = "-70px";
-        nav.style.transition = "0.4s ease-out" 
-        nav.style.position = "absolute";       
-    }
-}
+// let nav = document.querySelector(".nav");
+// window.onscroll = ()=>{
+//     if(document.documentElement.scrollTop >= 100)
+//     {
+//         nav.style.top = "0px";
+//         nav.style.position = "sticky";
+//         nav.style.transition = "0.4s ease-in"
+//     }
+//     else{
+//         nav.style.top = "-70px";
+//         nav.style.transition = "0.4s ease-out" 
+//         nav.style.position = "absolute";       
+//     }
+// }
