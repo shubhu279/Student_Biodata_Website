0945a8e0da6ac7a56c554d641980bec05ebf74a7
Saptarshi <saptarshimemari072@gmail.com>
2023-05-26 19:33:55 +0530

    Register Yourself

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,4 @@
 
 //Toggle-eye
 
-const togglePassword = document.querySelector("#Toggle-eye");
-const password = document.querySelector("#password");
 
-togglePassword.addEventListener("click", function () {
-  // toggle the type attribute
-  const type =
-    password.getAttribute("type") === "password" ? "text" : "password";
-  password.setAttribute("type", type);
-
-  // toggle the icon
-  this.classList.toggle("fa-eye");
-});
